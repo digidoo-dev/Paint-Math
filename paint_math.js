cbc6e5cd31ed189b2b61f7bc5c156c65dad12b4c
Piotr Ratkowski <piotr.ratkowski.dev@gmail.com>
2023-01-22 14:59:31 +0100

    More equations and cleaner code. Also alert messages alerting users of impossible equations.

--- paint_math.js
+++ paint_math.js
@@ -2,6 +2,13 @@ function setCellActive(row, column) {
     cellMatrix[row][column].classList.add("highlited-cell");
 }
 
+function setFourCellsActive(row, column) {
+    setCellActive(row, column);
+    setCellActive(row, column - 1);
+    setCellActive(row - 1, column);
+    setCellActive(row - 1, column - 1);
+}
+
 
 function getRowFromY(y) {
     return Math.round((6 - y) * 10);
@@ -13,12 +20,14 @@ function getColumnFromX(x) {
 
 const buttonReset = document.querySelector("#buttonreset");
 const mathCanvas = document.querySelector("#mathcanvas");
+
 const xyInputDiv = document.querySelector(".xyinput");
 const linearInput = document.querySelector(".linearinput");
-const quadraticInput = document.querySelector(".quadraticinput")
+const quadraticInput = document.querySelector(".quadraticinput");
+const exponentialInput = document.querySelector(".exponentialinput");
+const logarithmicInput = document.querySelector(".logarithmicinput");
+
 const buttonShow = document.querySelector("#showbutton");
-const inputX = document.querySelector("#inputX");
-const inputY = document.querySelector("#inputY");
 
 var mathType = 0;
 
@@ -49,22 +58,40 @@ for (var row = 0; row < height; row++) {
 }
 
 
+
+
+function setAllInputsInvisible() {
+
+    xyInputDiv.classList.add("invisible");
+    linearInput.classList.add("invisible");
+    quadraticInput.classList.add("invisible");
+    exponentialInput.classList.add("invisible");
+    logarithmicInput.classList.add("invisible");
+}
+
+
+
+
 $("#mathtypeselector").change(function() {
     mathType = $(this).val();
     if (mathType == 0){
+        setAllInputsInvisible();
         xyInputDiv.classList.remove("invisible");
-        linearInput.classList.add("invisible");
-        quadraticInput.classList.add("invisible");
-
+        
     } else if (mathType == 1) {
+        setAllInputsInvisible();
         linearInput.classList.remove("invisible");
-        xyInputDiv.classList.add("invisible");
-        quadraticInput.classList.add("invisible");
+        
     } else if (mathType == 2) {
+        setAllInputsInvisible();
         quadraticInput.classList.remove("invisible");
-        linearInput.classList.add("invisible");
-        xyInputDiv.classList.add("invisible");
 
+    } else if (mathType == 3) {
+        setAllInputsInvisible();
+        exponentialInput.classList.remove("invisible");
+    } else if (mathType == 4) {
+        setAllInputsInvisible();
+        logarithmicInput.classList.remove("invisible");
     }
 });
 
@@ -75,16 +102,22 @@ buttonReset.addEventListener("click", function(e) {
 
 buttonShow.addEventListener("click", function(e) {
     if (mathType == 0) {
-        const x = parseFloat(inputX.value);
-        const y = parseFloat(inputY.value);
+        
+        const x = parseFloat($("#inputX").val());
+
+        const y = parseFloat($("#inputY").val());
+
+        if (x < -7.9 || x > 7.9 || y < -5.9 || y > 5.9) {
+            alert("X has to be bigger than -8 but smaller than 8. Y has to be bigger than -6 but smaller than 6. Try again.");
+            return;
+        }
 
         row = getRowFromY(y);
         column = getColumnFromX(x);
 
-        setCellActive(row, column);
-        setCellActive(row, column - 1);
-        setCellActive(row - 1, column);
-        setCellActive(row - 1, column - 1);
+        
+        setFourCellsActive(row, column);
+
     }else if (mathType == 1) {
         const a = parseFloat($("#linearInputA").val());
         const b = parseFloat($("#linearInputB").val());
@@ -95,10 +128,8 @@ buttonShow.addEventListener("click", function(e) {
             row = getRowFromY(y);
             column = getColumnFromX(x);
             if (row > 0 && row <= 119 && column > 0 && column <= 159) {
-                setCellActive(row, column);
-                setCellActive(row, column - 1);
-                setCellActive(row - 1, column);
-                setCellActive(row - 1, column - 1);   
+                
+                setFourCellsActive(row, column);   
             }
 
             
@@ -108,6 +139,11 @@ buttonShow.addEventListener("click", function(e) {
         const b = parseFloat($("#quadraticInputB").val());
         const c = parseFloat($("#quadraticInputC").val());
 
+        if (a == 0) {
+            alert("A cannot be 0. Try again.");
+            return;
+        }
+
         for (var x = -7.9; x < 8; x += 0.1){
             var y = (a * x**2) + (b * x) + c;
             
@@ -115,17 +151,53 @@ buttonShow.addEventListener("click", function(e) {
             column = getColumnFromX(x);
             
             if (row > 0 && row <= 119 && column > 0 && column <= 159) {
-                setCellActive(row, column);
-                setCellActive(row, column - 1);
-                setCellActive(row - 1, column);
-                setCellActive(row - 1, column - 1);   
+                  
+                setFourCellsActive(row, column);
             }
         }
+    } else if (mathType == 3) {
+        const a = parseFloat($("#exponentialInputA").val())
 
+        if (!(a >0)) {
+            alert("A has to be bigger than 0. Try again.");
+            return;
+        }
+
+        for (var x = -7.9; x < 8; x += 0.1){
+            var y = a**x;
+
+            row = getRowFromY(y);
+            column = getColumnFromX(x);
+
+            if (row > 0 && row <= 119 && column > 0 && column <= 159) {
+               
+                setFourCellsActive(row, column);
+            }
+        }
+    } else if (mathType==4) {
+        const a = parseFloat($("#logarithmicInputA").val())
         
+        if (a <= 0 || a == 1) {
+            alert("A has to be bigger than 0, and cannot be equal to 1. Try again.");
+            return;
+        }
+
+        for (var x = -7.9; x < 8; x += 0.1){
+            var y = log(a, x);
+
+            row = getRowFromY(y);
+            column = getColumnFromX(x);
+
+            if (row > 0 && row <= 119 && column > 0 && column <= 159) {
+               
+                setFourCellsActive(row, column);
+            }
+        }
     }
     
     
-
+    function log(base, number) {
+        return Math.log(number) / Math.log(base);
+    }
 
 });
