3503d62ea9ab0b3f34b13c457bc514d32f84f5d8
Piotr Ratkowski <piotr.ratkowski.dev@gmail.com>
2023-01-25 01:26:12 +0100

    Complete rework to work with HTML Canvas element. The graphs are now really drawn lines and not just cell dots in a huge table. Increased the size of coordinate system.

--- paint_math.js
+++ paint_math.js
@@ -1,25 +1,4 @@
-function setCellActive(row, column) {
-    cellMatrix[row][column].classList.add("highlited-cell");
-}
-
-function setFourCellsActive(row, column) {
-    setCellActive(row, column);
-    setCellActive(row, column - 1);
-    setCellActive(row - 1, column);
-    setCellActive(row - 1, column - 1);
-}
-
-
-function getRowFromY(y) {
-    return Math.round((6 - y) * 10);
-}
-
-function getColumnFromX(x) {
-    return Math.round((8 + x) * 10);
-}
-
 const buttonReset = document.querySelector("#buttonreset");
-const mathCanvas = document.querySelector("#mathcanvas");
 
 const xyInputDiv = document.querySelector(".xyinput");
 const linearInput = document.querySelector(".linearinput");
@@ -30,33 +9,6 @@ const logarithmicInput = document.querySelector(".logarithmicinput");
 const buttonShow = document.querySelector("#showbutton");
 
 var mathType = 0;
-
-const width = 160;
-const height = 120;
-
-const cellSizePixels = 3;
-
-const canvasWidthPixels = 480;
-const canvasHeightPixels = 360;
-
-const canvasWidthUnits = 16;
-const canvasHeightUnits = 12;
-
-var cellMatrix = [];
-
-for (var row = 0; row < height; row++) {
-    var tableRow = mathCanvas.insertRow();
-    tableRow.setAttribute("id", "row-" + row);
-    var rowList = [];
-    for (var column = 0; column < width; column++) {
-        tableCell = tableRow.insertCell();
-        tableCell.setAttribute("id", "cell-" + row + "-" + column);
-
-        rowList.push(tableCell);
-    }
-    cellMatrix.push(rowList);
-}
-
 
 
 
@@ -96,44 +48,25 @@ $("#mathtypeselector").change(function() {
 });
 
 buttonReset.addEventListener("click", function(e) {
-    $(".highlited-cell").toggleClass("highlited-cell");
+    ctx.clearRect(0, 0, width, height);
+    drawCoordinateSystem();
 });
 
 
 buttonShow.addEventListener("click", function(e) {
     if (mathType == 0) {
         
-        const x = parseFloat($("#inputX").val());
+        const xCoordinate = parseFloat($("#inputX").val());
+        const yCoordinate = parseFloat($("#inputY").val());
 
-        const y = parseFloat($("#inputY").val());
-
-        if (x < -7.9 || x > 7.9 || y < -5.9 || y > 5.9) {
-            alert("X has to be bigger than -8 but smaller than 8. Y has to be bigger than -6 but smaller than 6. Try again.");
-            return;
-        }
-
-        row = getRowFromY(y);
-        column = getColumnFromX(x);
-
-        
-        setFourCellsActive(row, column);
+        drawPoint(xCoordinate, yCoordinate);
 
     }else if (mathType == 1) {
         const a = parseFloat($("#linearInputA").val());
         const b = parseFloat($("#linearInputB").val());
 
-        for (var x = -7.9; x < 8; x += 0.1) {
-            var y = a * x + b
+        drawLinearFunction(a, b);
 
-            row = getRowFromY(y);
-            column = getColumnFromX(x);
-            if (row > 0 && row <= 119 && column > 0 && column <= 159) {
-                
-                setFourCellsActive(row, column);   
-            }
-
-            
-        }
     } else if(mathType == 2) {
         const a = parseFloat($("#quadraticInputA").val());
         const b = parseFloat($("#quadraticInputB").val());
@@ -144,36 +77,21 @@ buttonShow.addEventListener("click", function(e) {
             return;
         }
 
-        for (var x = -7.9; x < 8; x += 0.1){
-            var y = (a * x**2) + (b * x) + c;
-            
-            row = getRowFromY(y);
-            column = getColumnFromX(x);
-            
-            if (row > 0 && row <= 119 && column > 0 && column <= 159) {
-                  
-                setFourCellsActive(row, column);
-            }
-        }
+        drawQuadraticFunction(a, b, c);
+
     } else if (mathType == 3) {
         const a = parseFloat($("#exponentialInputA").val())
 
         if (!(a >0)) {
             alert("A has to be bigger than 0. Try again.");
             return;
+        } else if (a>6) {
+            alert("Please use an A of max 6. (That's the most a web browser can handle.)");
+            return;
         }
 
-        for (var x = -7.9; x < 8; x += 0.1){
-            var y = a**x;
+        drawExponentialFunction(a);
 
-            row = getRowFromY(y);
-            column = getColumnFromX(x);
-
-            if (row > 0 && row <= 119 && column > 0 && column <= 159) {
-               
-                setFourCellsActive(row, column);
-            }
-        }
     } else if (mathType==4) {
         const a = parseFloat($("#logarithmicInputA").val())
         
@@ -182,22 +100,165 @@ buttonShow.addEventListener("click", function(e) {
             return;
         }
 
-        for (var x = -7.9; x < 8; x += 0.1){
-            var y = log(a, x);
-
-            row = getRowFromY(y);
-            column = getColumnFromX(x);
-
-            if (row > 0 && row <= 119 && column > 0 && column <= 159) {
-               
-                setFourCellsActive(row, column);
-            }
-        }
+        drawLogarithmicFunction(a);
     }
+});
+
+
+function drawCoordinateSystem() {
     
+    // X axis
+    ctx.beginPath();
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height /2);
+    ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+    ctx.stroke();
+    // arrow
+    ctx.beginPath();
+    ctx.moveTo(width, height / 2);
+    ctx.lineTo(width - 10, (height / 2) + 10);
+    ctx.lineTo(width - 10, (height / 2) - 10);
+    ctx.fill();
+
     
+    // Y axis
+    ctx.beginPath();
+    ctx.moveTo(width / 2, height);
+    ctx.lineTo(width / 2, 0);
+    ctx.stroke();
+    // arrow
+    ctx.beginPath();
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2 - 10, 10);
+    ctx.lineTo(width / 2 + 10, 10);
+    ctx.fill();
+
+
+    // lines and numbers on X axis
+    var i = -10;
+    for (var x = 0; x <= width; x += width / 20) {
+        ctx.beginPath();
+        ctx.moveTo(x, height / 2 - 5);
+        ctx.lineTo(x, height / 2 + 5);
+        ctx.stroke();
+        ctx.font = "10px arial";
+        if (x != 0 && x != width && i != 0) ctx.fillText(i, x - 4, height / 2 + 15);
+        i++;
+    }
+
+
+    // lines and numbers on Y axis
+    i = -10;
+    for (var y = height; y >= 0; y -= height / 20) {
+        ctx.beginPath();
+        ctx.moveTo(width / 2 - 5, y);
+        ctx.lineTo(width / 2 + 5, y);
+        ctx.stroke();
+        ctx.font = "10px arial";
+        if (y != height && y!= 0 && i != 0) ctx.fillText(i, width / 2 - 15, y + 5);
+        i++;
+    }
+
+
+    // Faint lines on whole canvas
+    for (var x = 0; x <= width; x += width / 20) {
+        ctx.beginPath();
+        ctx.moveTo(x, height);
+        ctx.lineTo(x, 0);
+        ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+        ctx.stroke();   
+    }
+    for (var y = height; y >= 0; y -= height / 20 ) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+        ctx.stroke();
+    }
+
+
+    // 0 in the middle
+    ctx.font = "10px arial";
+    ctx.fillText(0, width / 2 - 10, height / 2 + 10);
+}
+
+function getCanvasPositionX(coordinateX) {
+    return (10 + coordinateX) * 25;
+}
+
+function getCanvasPositionY(coordinateY) {
+    return (10 - coordinateY) * 25;
+}
+
+function drawPoint(xCoordinate, yCoordinate) {
+    const canvasX = getCanvasPositionX(xCoordinate);
+    const canvasY = getCanvasPositionY(yCoordinate);
+    ctx.fillRect(canvasX - 2, canvasY -2, 4, 4);
+    ctx.fillText("("+xCoordinate+","+yCoordinate+")", canvasX - 10, canvasY + 10);
+}
+
+function drawLinearFunction(a, b) {
+    ctx.beginPath();
+    var x = -10; 
+    var y = a * x + b;
+    ctx.moveTo(getCanvasPositionX(x), getCanvasPositionY(y));
+    x = 10;
+    y = a * x + b;
+    ctx.lineTo(getCanvasPositionX(x), getCanvasPositionY(y));
+    ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+    ctx.stroke();
+}
+
+function drawQuadraticFunction(a, b, c) {
+    for (var x = -10; x <= 10; x += 0.1 ) {
+        var y = (a * (x**2)) + (b * x) + c;
+        if (x == -10) ctx.moveTo(getCanvasPositionX(x), getCanvasPositionY(y));
+        else ctx.lineTo(getCanvasPositionX(x), getCanvasPositionY(y));
+    }
+    ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+    ctx.stroke();
+}
+
+function drawExponentialFunction(a) {
+    for (var x = -10; x <= 10; x += 0.1){
+        var y = a**x;
+        if (x == -10) ctx.moveTo(getCanvasPositionX(x), getCanvasPositionY(y));
+        else ctx.lineTo(getCanvasPositionX(x), getCanvasPositionY(y));
+    }
+    ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+    ctx.stroke();
+}
+
+function drawLogarithmicFunction(a) {
+    for (var x = 0.05; x <= 10; x += 0.05){
+
+        var y = log(a, x);
+        if (x == 0.05) ctx.moveTo(getCanvasPositionX(x), getCanvasPositionY(y));
+        else ctx.lineTo(getCanvasPositionX(x), getCanvasPositionY(y));
+    }
+    ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+    ctx.stroke();
+
     function log(base, number) {
         return Math.log(number) / Math.log(base);
     }
 
-});
+}
+
+const mathCanvas = document.querySelector("#mathcanvas");
+const ctx = mathCanvas.getContext("2d");
+
+const width = mathCanvas.width;
+const height = mathCanvas.height;
+
+drawCoordinateSystem();
+
+
+
+
+
+
+
+
+
+
